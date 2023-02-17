92718e8394c27d7c501fbae079d66d4a65b81cd2
Raja Prism <rajaprismsagar001@gmail.com>
2023-02-17 11:28:09 +0530

    Done till checkpoint 1 part 2

--- Assignment2-COP290/part2/include/hm.h
+++ Assignment2-COP290/part2/include/hm.h
@@ -15,7 +15,7 @@ struct hashmap_s
     struct lock* lk[SZ];
 };
 
-int hash_code_map(char* key)
+int hash_code_map(const char* key)
 {
     int val=0;
     int i=0;
@@ -26,17 +26,17 @@ int hash_code_map(char* key)
         i++;
         val=val%(SZ);
     }
-    return val;
+    return val%(SZ);
 }
 
 int hashmap_create(struct hashmap_s *const out_hashmap)  // Initialize a hashmap
 {
         for(int i=0;i<SZ;i++)
         {
-        out_hashmap->table[i] = malloc(sizeof(struct list));
+        out_hashmap->table[i]=(struct list*)malloc(sizeof(struct list));
         out_hashmap->table[i]->head=NULL;
         out_hashmap->table[i]->tail=NULL;
-    }
+        }
 }
 int hashmap_put(struct hashmap_s *const hashmap, const char* key, void* data)  // Set value of the key as data in hashmap. You can use any method to resolve conflicts. Also write your own hashing function
 {
@@ -44,19 +44,20 @@ int hashmap_put(struct hashmap_s *const hashmap, const char* key, void* data)  /
     struct list* get_list=hashmap->table[key_map];
     struct listentry* head=get_list->head;
     struct listentry* tail=get_list->tail;
-    while (head!=NULL && strcmp(((struct hashmap_element_s*)(head->data)->key),key)!=0)
+    while (head!=NULL && strcmp(((struct hashmap_element_s*)(head->data))->key,key)!=0)
     { 
         head=head->next;
     }
     if(head==NULL)
     {
-        struct listentry* new_entry= malloc(sizeof(struct listentry));
-        struct hashmap_element_s* key_data= malloc(sizeof(struct hashmap_element_s));
+        struct listentry* new_entry=(struct listentry*) malloc(sizeof(struct listentry));
+        struct hashmap_element_s* key_data=(struct hashmap_element_s*) malloc(sizeof(struct hashmap_element_s));
         new_entry->data=key_data;
-        key_data->key=malloc((strlen(key)+1)*sizeof(char));
+        key_data->key=(char*)malloc(strlen(key)+1);
         strcpy(key_data->key,key);
         key_data->data=data;
         new_entry->next=NULL;
+        new_entry->prev=NULL;
         if(get_list->head==NULL && get_list->tail==NULL)
         {
             get_list->head=new_entry;
@@ -66,20 +67,20 @@ int hashmap_put(struct hashmap_s *const hashmap, const char* key, void* data)  /
         {
             new_entry->prev=tail;
             tail->next=new_entry;
-            tail=new_entry;
+            get_list->tail=new_entry;
         }
-        else
-        {
-            ((struct hash_element_S*)(head->data))->data=data;
-        }
+    }
+    else
+    {
+        ((struct hash_element_s*)(head->data))->data=data;
     }
 }
 void* hashmap_get(struct hashmap_s *const hashmap, const char* key)    // Fetch value of a key from hashmap
 {
     int code_key=hash_code_map(key);
-    struct list* get_list=hashmap[code_key];
+    struct list* get_list=hashmap->table[code_key];
     struct listentry* head=get_list->head;
-    while (head!=NULL && strcmp(((struct hash_element_s*)(head->data)->key),key)!=0)
+    while (head!=NULL && strcmp((((struct hash_element_s*)(head->data))->key),key)!=0)
     {
         head=head->next;
     }
@@ -96,7 +97,16 @@ void* hashmap_get(struct hashmap_s *const hashmap, const char* key)    // Fetch 
 }
 void hashmap_iterator(struct hashmap_s* const hashmap,int (*f)(struct hashmap_element_s *const))  // Execute argument function on each key-value pair in hashmap
 {
+    for(int i=0;i<SZ;i++)
+    {
+        struct listentry* head=hashmap->table[i]->head;
+        struct listentry* tail=hashmap->table[i]->tail;
 
+        while(head!=NULL)
+        {
+            f((struct hash_element_s*)(head->data));
+        }        
+    }
 }
 
 int acquire_bucket(struct hashmap_s *const hashmap, const char* key) // Acquire lock on a hashmap slot
